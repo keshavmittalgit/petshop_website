360e354d094012e789ffd9fef09492d1f0382994
keshavmittalgit <keshavmittal7974@gmail.com>
2025-02-19 05:59:13 +0530

    respoive navbar working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,4 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-import { Switch } from "@/components/ui/switch";
+
 import Navbar  from "@/components/ui/navbar";
 
 
